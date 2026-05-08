c2fc895006653284b9ccc3b4b185a5d9c363a422
zzy <2218847957@qq.com>
2026-05-08 16:46:05 +0800

    Initial Commit

---
+++ Venne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Venne
+//
+//  Created by yangyang on 2026/5/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Venne/VenneApp.swift
@@ -0,0 +1,17 @@
+//
+//  VenneApp.swift
+//  Venne
+//
+//  Created by yangyang on 2026/5/8.
+//
+
+import SwiftUI
+
+@main
+struct VenneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
